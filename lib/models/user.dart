ea29b87d4376485b9013daa1ceded03d6a2752fa
kiraitami <l.kiraitami@gmail.com>
2020-02-02 14:09:35 -0300

    L #2 - Making the Main Screen

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -24,7 +24,6 @@ class User extends Model {
               notifyListeners();
             }
     ).catchError( (error) {
-      print('aaaaaaaaaaaaaaaaaaaaaaaaaa');
       print(error);
       notifyListeners();
     });
